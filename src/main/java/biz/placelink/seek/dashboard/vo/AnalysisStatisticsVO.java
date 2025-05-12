eb18c2d01d323b00d6fdf01b9759ce71324b4943
s2 <eseungsu.dev@gmail.com>
2025-05-12 16:54:04 +0900

    실시간 분석 현황 수정정

--- src/main/java/biz/placelink/seek/dashboard/vo/AnalysisStatisticsVO.java
+++ src/main/java/biz/placelink/seek/dashboard/vo/AnalysisStatisticsVO.java
@@ -44,7 +44,7 @@ public class AnalysisStatisticsVO extends DefaultVO {
     /* 탐지 현황 - 해외 낮음 */
     private int otherLowCount;
 
-    /* 실시간 분석 현황 - 분 */
+    /* 실시간 분석 현황 - 분 (YYYYMMDDHH24MI) */
     private String minuteGroup;
 
     /* 민감 정보 검출 타입 공통코드 */
@@ -52,101 +52,180 @@ public class AnalysisStatisticsVO extends DefaultVO {
     /* 민감 정보 탐지 횟수 */
     private int hitCount;
 
-
-    public int getAnalysisCount() { return analysisCount; }
+    public int getAnalysisCount() {
+        return analysisCount;
+    }
 
     public void setAnalysisCount(int analysisCount) {
         this.analysisCount = analysisCount;
     }
 
-    public int getDetectionCount() { return detectionCount; }
+    public int getDetectionCount() {
+        return detectionCount;
+    }
 
     public void setDetectionCount(int detectionCount) {
         this.detectionCount = detectionCount;
     }
 
-    public int getMaskingCount() { return maskingCount; }
+    public int getMaskingCount() {
+        return maskingCount;
+    }
 
-    public void setMaskingCount(int maskingCount) { this.maskingCount = maskingCount; }
+    public void setMaskingCount(int maskingCount) {
+        this.maskingCount = maskingCount;
+    }
 
-    public int getUnmaskingCount() { return unmaskingCount; }
+    public int getUnmaskingCount() {
+        return unmaskingCount;
+    }
 
-    public void setUnmaskingCount(int unmaskingCount) { this.unmaskingCount = unmaskingCount; }
+    public void setUnmaskingCount(int unmaskingCount) {
+        this.unmaskingCount = unmaskingCount;
+    }
 
-    public int getTotalAnalysisCount() { return totalAnalysisCount; }
+    public int getTotalAnalysisCount() {
+        return totalAnalysisCount;
+    }
 
-    public void setTotalAnalysisCount(int totalAnalysisCount) { this.totalAnalysisCount = totalAnalysisCount; }
+    public void setTotalAnalysisCount(int totalAnalysisCount) {
+        this.totalAnalysisCount = totalAnalysisCount;
+    }
 
-    public int getTotalDetectionCount() { return totalDetectionCount; }
+    public int getTotalDetectionCount() {
+        return totalDetectionCount;
+    }
 
-    public void setTotalDetectionCount(int totalDetectionCount) { this.totalDetectionCount = totalDetectionCount; }
+    public void setTotalDetectionCount(int totalDetectionCount) {
+        this.totalDetectionCount = totalDetectionCount;
+    }
 
-    public int getTotalMaskingCount() { return totalMaskingCount; }
+    public int getTotalMaskingCount() {
+        return totalMaskingCount;
+    }
 
-    public void setTotalMaskingCount(int totalMaskingCount) { this.totalMaskingCount = totalMaskingCount; }
+    public void setTotalMaskingCount(int totalMaskingCount) {
+        this.totalMaskingCount = totalMaskingCount;
+    }
 
-    public int getTotalUnmaskingCount() { return totalUnmaskingCount; }
+    public int getTotalUnmaskingCount() {
+        return totalUnmaskingCount;
+    }
 
-    public void setTotalUnmaskingCount(int totalUnmaskingCount) { this.totalUnmaskingCount = totalUnmaskingCount; }
+    public void setTotalUnmaskingCount(int totalUnmaskingCount) {
+        this.totalUnmaskingCount = totalUnmaskingCount;
+    }
 
+    public int getTotalCountryCount() {
+        return totalCountryCount;
+    }
 
-    public int getTotalCountryCount() { return totalCountryCount; }
+    public void setTotalCountryCount(int totalCountryCount) {
+        this.totalCountryCount = totalCountryCount;
+    }
 
-    public void setTotalCountryCount(int totalCountryCount) { this.totalCountryCount = totalCountryCount; }
+    public int getTotalKoreaCount() {
+        return totalKoreaCount;
+    }
 
-    public int getTotalKoreaCount() { return totalKoreaCount; }
+    public void setTotalKoreaCount(int totalKoreaCount) {
+        this.totalKoreaCount = totalKoreaCount;
+    }
 
-    public void setTotalKoreaCount(int totalKoreaCount) { this.totalKoreaCount = totalKoreaCount; }
+    public int getTotalOtherCountryCount() {
+        return totalOtherCountryCount;
+    }
 
-    public int getTotalOtherCountryCount() { return totalOtherCountryCount; }
+    public void setTotalOtherCountryCount(int totalOtherCountryCount) {
+        this.totalOtherCountryCount = totalOtherCountryCount;
+    }
 
-    public void setTotalOtherCountryCount(int totalOtherCountryCount) { this.totalOtherCountryCount = totalOtherCountryCount; }
+    public int getKoreaCount() {
+        return koreaCount;
+    }
 
-    public int getKoreaCount() { return koreaCount; }
+    public void setKoreaCount(int koreaCount) {
+        this.koreaCount = koreaCount;
+    }
 
-    public void setKoreaCount(int koreaCount) { this.koreaCount = koreaCount; }
+    public int getOtherCountryCount() {
+        return otherCountryCount;
+    }
 
-    public int getOtherCountryCount() { return otherCountryCount; }
+    public void setOtherCountryCount(int otherCountryCount) {
+        this.otherCountryCount = otherCountryCount;
+    }
 
-    public void setOtherCountryCount(int otherCountryCount) { this.otherCountryCount = otherCountryCount; }
+    public int getKoreaHighCount() {
+        return koreaHighCount;
+    }
 
-    public int getKoreaHighCount() { return koreaHighCount; }
+    public void setKoreaHighCount(int koreaHighCount) {
+        this.koreaHighCount = koreaHighCount;
+    }
 
-    public void setKoreaHighCount(int koreaHighCount) { this.koreaHighCount = koreaHighCount; }
+    public int getKoreaMidCount() {
+        return koreaMidCount;
+    }
 
-    public int getKoreaMidCount() { return koreaMidCount; }
+    public void setKoreaMidCount(int koreaMidCount) {
+        this.koreaMidCount = koreaMidCount;
+    }
 
-    public void setKoreaMidCount(int koreaMidCount) { this.koreaMidCount = koreaMidCount; }
+    public int getKoreaLowCount() {
+        return koreaLowCount;
+    }
 
-    public int getKoreaLowCount() { return koreaLowCount; }
+    public void setKoreaLowCount(int koreaLowCount) {
+        this.koreaLowCount = koreaLowCount;
+    }
 
-    public void setKoreaLowCount(int koreaLowCount) { this.koreaLowCount = koreaLowCount; }
+    public int getOtherHighCount() {
+        return otherHighCount;
+    }
 
-    public int getOtherHighCount() { return otherHighCount; }
+    public void setOtherHighCount(int otherHighCount) {
+        this.otherHighCount = otherHighCount;
+    }
 
-    public void setOtherHighCount(int otherHighCount) { this.otherHighCount = otherHighCount; }
+    public int getOtherMidCount() {
+        return otherMidCount;
+    }
 
-    public int getOtherMidCount() { return otherMidCount; }
+    public void setOtherMidCount(int otherMidCount) {
+        this.otherMidCount = otherMidCount;
+    }
 
-    public void setOtherMidCount(int otherMidCount) { this.otherMidCount = otherMidCount; }
+    public int getOtherLowCount() {
+        return otherLowCount;
+    }
 
-    public int getOtherLowCount() { return otherLowCount; }
+    public void setOtherLowCount(int otherLowCount) {
+        this.otherLowCount = otherLowCount;
+    }
 
-    public void setOtherLowCount(int otherLowCount) { this.otherLowCount = otherLowCount; }
+    public String getMinuteGroup() {
+        return minuteGroup;
+    }
 
+    public void setMinuteGroup(String minuteGroup) {
+        this.minuteGroup = minuteGroup;
+    }
 
-    public String getMinuteGroup() { return minuteGroup; }
+    public String getDetectionTypeCcd() {
+        return detectionTypeCcd;
+    }
 
-    public void setMinuteGroup(String minuteGroup) { this.minuteGroup = minuteGroup; }
+    public void setDetectionTypeCcd(String detectionTypeCcd) {
+        this.detectionTypeCcd = detectionTypeCcd;
+    }
 
+    public int getHitCount() {
+        return hitCount;
+    }
 
-    public String getDetectionTypeCcd() { return detectionTypeCcd; }
-
-    public void setDetectionTypeCcd(String detectionTypeCcd) { this.detectionTypeCcd = detectionTypeCcd; }
-
-    public int getHitCount() { return hitCount; }
-
-    public void setHitCount(int hitCount) { this.hitCount = hitCount; }
-
+    public void setHitCount(int hitCount) {
+        this.hitCount = hitCount;
+    }
 
 }
